fa25d92cf607c44d036d5366801879cdf1d158f9
Jessica Oliveira <jessie_oliveira@icloud.com>
2018-12-11 11:51:56 -0500

    Added: ViewPager on main screen with tabs.

--- LetsChill_FinalProject/app/src/main/java/com/example/prajwalramamurthy/letschill_finalproject/activities/MainActivity.java
+++ LetsChill_FinalProject/app/src/main/java/com/example/prajwalramamurthy/letschill_finalproject/activities/MainActivity.java
@@ -3,13 +3,28 @@ package com.example.prajwalramamurthy.letschill_finalproject.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabItem;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 import com.example.prajwalramamurthy.letschill_finalproject.R;
-import com.example.prajwalramamurthy.letschill_finalproject.fragments.MainFragment;
+import com.example.prajwalramamurthy.letschill_finalproject.utility.MainPageAdapter;
 
-public class MainActivity extends AppCompatActivity implements MainFragment.MainFragmentInterface {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Variables
+    private FloatingActionButton mFab;
+    private MainPageAdapter mTabAdapter;
+    private TabItem mTabToday;
+    private TabItem mTabUpcoming;
+    private TabItem mTabPast;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,19 +32,66 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Main
         setContentView(R.layout.activity_main);
         setTitle("Events");
 
-        getSupportFragmentManager().beginTransaction().add(R.id.main_frame, MainFragment.newInstance()).commit();
-    }
+        // Find Views
+        mFab = findViewById(R.id.fab_activity);
+        mTabToday = findViewById(R.id.tab_today);
+        mTabUpcoming = findViewById(R.id.tab_upcoming);
+        mTabPast = findViewById(R.id.tab_past);
+        mViewPager = findViewById(R.id.viewPager_tabs);
+        mTabLayout = findViewById(R.id.tablayout_events);
 
-    @Override
-    public void moveToCreateEventActivity() {
+        // Assign the click listener to the floating button
+        mFab.setOnClickListener(this);
 
-        // Move to "CreateEventActivity"
-        Intent mCreateEventIntent = new Intent(MainActivity.this, CreateEventActivity.class);
-        startActivity(mCreateEventIntent);
+        // Assign the adapter to the view pager that will display the screen for each tab item
+        mTabAdapter = new MainPageAdapter(getSupportFragmentManager(), mTabLayout.getTabCount());
+        mViewPager.setAdapter(mTabAdapter);
+
+        // Manage what to display when a tab is selected
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                mViewPager.setCurrentItem(tab.getPosition());
+
+
+                Log.d("test", "onTabSelected: tab was selected: " + tab.getText());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.fab_activity:
+
+                // Move to "CreateEventActivity"
+                Intent mCreateEventIntent = new Intent(MainActivity.this, CreateEventActivity.class);
+                startActivity(mCreateEventIntent);
+
+                break;
+        }
     }
 }
--- LetsChill_FinalProject/app/src/main/java/com/example/prajwalramamurthy/letschill_finalproject/fragments/TabUpcomingFragment.java
+++ LetsChill_FinalProject/app/src/main/java/com/example/prajwalramamurthy/letschill_finalproject/fragments/TabUpcomingFragment.java
@@ -1,7 +1,14 @@
 package com.example.prajwalramamurthy.letschill_finalproject.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.prajwalramamurthy.letschill_finalproject.R;
 
 public class TabUpcomingFragment extends Fragment {
 
@@ -12,5 +19,17 @@ public class TabUpcomingFragment extends Fragment {
         TabUpcomingFragment fragment = new TabUpcomingFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab_upcoming, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        
     }
 }
